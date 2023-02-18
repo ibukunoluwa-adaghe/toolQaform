87c55f113e4d7c73c488be3d3a5fc48d8a0266b1
ibukunoluwa-adaghe <78601819+ibukunoluwa-adaghe@users.noreply.github.com>
2023-02-18 10:40:44 +0100

    new fields on form

--- cypress/integration/toolsAss/toolsForm.spec.js
+++ cypress/integration/toolsAss/toolsForm.spec.js
@@ -7,6 +7,10 @@ const type = new regForm();
 const button = new regButton();
 const data = require ('../../fixtures/data.json')
 
+beforeEach(() => {
+    cy.viewport("iphone-8")
+});
+
 describe ('myForm Module', function(){
     it('My registration form', () => {
         cy.visit(Cypress.env('baseUrl'))
@@ -18,5 +22,21 @@ describe ('myForm Module', function(){
         type.addressField(data.validFormDetails.address)
         button.genderRadioButton()
         button.hobbyCheckButton()
+        //Select first subject
+        type.subjectField(data.subjectList.subject1)
+        button.subjectSelection()
+        cy.wait(1000)
+        // select second subject
+        type.subjectField(data.subjectList.subject2)
+        button.subjectSelection()
+
+        button.stateDropdown()
+        //select from statedropdown
+        cy.get('.css-11unzgr').find('#react-select-3-option-1').click({force: true})
+
+        button.cityDropdown()
+        //select from city dropdown
+        cy.get('.css-11unzgr').find('#react-select-4-option-0').click({force: true})
+
     });
 })
--- cypress/support/pageObjects/buttonForm.js
+++ cypress/support/pageObjects/buttonForm.js
@@ -5,6 +5,15 @@ class regButton {
     hobbyCheckButton() {
         return cy.get('#hobbies-checkbox-3.custom-control-input').click({force: true})
     }
+    subjectSelection() {
+        return cy.get('#react-select-2-option-0').click()
+    }
+    stateDropdown() {
+        return cy.get('#state > .css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click({force: true})
+    }
+    cityDropdown() {
+        return cy.get('#city > .css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer > .css-19bqh2r').click({force: true})
+    }
 
 }
 export default regButton;
